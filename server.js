7789f43c1f030772bc3c2d9e9e5aff7125107d66
Revanth <revanth@example.com>
2026-06-10 10:32:31 +0530

    Backend connected

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const mongoUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crm_db';
 console.log("Mongo URI exists:", !!process.env.MONGODB_URI);
 console.log("Mongo URI value:", process.env.MONGODB_URI ? "FOUND" : "MISSING");
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
